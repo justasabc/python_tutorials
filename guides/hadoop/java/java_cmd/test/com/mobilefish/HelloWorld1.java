8860064a0c54452a1b9204edc40616350b4d219e
zunlin1234 <zunlin1234@gmail.com>
2013-10-25 02:01:22 -0700

    remove guides

--- guides/hadoop/java/java_cmd/test/com/mobilefish/HelloWorld1.java
+++
@@ -1,7 +0,0 @@
-package com.mobilefish;
-
-public class HelloWorld1 {
-	public static void main(String args[]) {
-		System.out.println("Hello World1");
-	}
-}
--- guides/hadoop/java/java_cmd/test/test/com/HelloWorld2.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-public class HelloWorld2 {
-	public static void main(String args[]) {
-		System.out.println("Hello World2");
-	}
-}
